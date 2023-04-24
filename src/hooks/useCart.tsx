39a0feb85fcd4c2e6556e6385b8d910031b6b984
Thaysa Lacerda <t_thaysa.lacerda@hapvida.com.br>
2023-04-24 15:32:55 -0400

    remoção de item do carrinho

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -71,9 +71,19 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
 
   const removeProduct = (productId: number) => {
     try {
-      // TODO
+      const updatedCart = [...cart];
+      const productIndex = updatedCart.findIndex(product => product.id === productId);
+
+      if(productIndex >= 0){
+        updatedCart.splice(productIndex, 1);
+        setCart(updatedCart);
+        localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart));
+      }
+      else{
+        throw Error();
+      }
     } catch {
-      // TODO
+      toast.error('Erro na remoção do produto');
     }
   };
 
