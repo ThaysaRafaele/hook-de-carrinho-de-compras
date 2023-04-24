55eb9dd4176eb50cdaf4efd8ca50c3f6f1cede05
Thaysa Lacerda <t_thaysa.lacerda@hapvida.com.br>
2023-04-24 15:45:43 -0400

    atualizar produtos no carrinho

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -92,9 +92,32 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     amount,
   }: UpdateProductAmount) => {
     try {
-      // TODO
+      if(amount <= 0) {
+        return;
+      }
+      
+      const stock = await api.get(`/stock/${productId}`);
+      const stockAmount =  stock.data.amount;
+
+      if(amount > stockAmount){
+        toast.error('Quantidade solicitada fora de estoque');
+        return;
+      }
+
+      const updatedCart = [...cart];
+      const productExists = updatedCart.find(product => product.id === productId);
+
+      if(productExists){
+        productExists.amount = amount;
+        setCart(updatedCart);
+        localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart));
+      }
+      else{
+        throw Error();
+      }
+
     } catch {
-      // TODO
+      toast.error('Erro na alteração de quantidade do produto');
     }
   };
 
